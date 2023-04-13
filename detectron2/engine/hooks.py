12eda0607e23fa5af60c20508abe5096dac1dcfc
Animesh Agrawal <animesh.a.777@gmail.com>
2023-04-13 15:03:47 -0400

    bug in detectron2 code fixed temporarily

--- detectron2/engine/hooks.py
+++ detectron2/engine/hooks.py
@@ -324,7 +324,7 @@ class LRScheduler(HookBase):
         self._scheduler = scheduler
 
     def before_train(self):
-        self._optimizer = self._optimizer or self.trainer.optimizer
+        self._optimizer = self._optimizer #bug? doesn't exist or self.trainer.optimizer
         if isinstance(self.scheduler, ParamScheduler):
             self._scheduler = LRMultiplier(
                 self._optimizer,
